376cf92368878571b149df35d2ca4eeb3aae5742
jankleinert <jkleiner@redhat.com>
2019-08-22 15:23:59 -0400

    Updated files to use new app

--- index.js
+++ index.js
@@ -2,22 +2,74 @@ const express = require('express');
 const app = express();
 const host = process.env.IP  || '0.0.0.0';
 const port = process.env.PORT || 8080;
+const dbUri = process.env.uri || 'mongodb://localhost:27017';
+const dbName = process.env.database_name || 'sampledb';
+const dbUser = process.env.username;
+const dbPassword = process.env.password;
+const mongo = require('mongodb').MongoClient;
 
-var ridesList = {
-    rides: [
-        { id: "123", name: "Compile Driver", wait: 30},
-        { id: "234", name: "Wild West", wait: 5}
-    ]    
-}
+app.get('/ticketNumber', function(req, res, next) {
+	res.send({success: true, result: 999});
+});
 
-app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-  	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  	next();
+app.get('/initdb', function (req, res, next) {
+	var ridesList;
+	let connectString = dbUri; 
+	console.log(dbName);
+	console.log('connect string: ' + connectString);
+	if (dbUser && dbPassword) {
+		console.log('have username and password ' + dbUser + ' ' + dbPassword);
+		connectString = dbUri.replace('mongodb://', 'mongodb://' + dbUser + ':' + dbPassword + '@');
+		connectString += '/' + dbName;
+		console.log('connect string after replace: ' + connectString);
+		
+	}
+	console.log('connect string after if ' + connectString);
+	mongo.connect(connectString, (err, client) => {
+		if (err) {
+		  console.error(err)
+		  return
+		}
+		console.log(connectString);
+		const db = client.db(dbName);
+		const collection = db.collection('rides');
+		collection.insertMany([{id: '123', name: 'Compile Driver', wait: 30}, {id: '234', name: 'Wild West', wait: 5}], (err, result) => {
+			console.log('err:' + err, ' result: ' + result);
+		});
+		
+
+		collection.find().toArray((err, items) => {
+			ridesList = items;
+			console.log(items);
+		});
+	  });
+	console.log(ridesList);	
+	res.send({success: true, result: ridesList});
 });
 
 app.get('/allrides/', function (req, res, next) {
-		
+	var ridesList;
+	let connectString = dbUri;
+	if (dbUser && dbPassword) {
+		console.log('have username and password ' + dbUser + ' ' + dbPassword);
+		connectString = dbUri.replace('mongodb://', 'mongodb://' + dbUser + ':' + dbPassword + '@');
+		connectString += '/' + dbName;
+		console.log(connectString);
+	}
+	mongo.connect(connectString, (err, client) => {
+		if (err) {
+		  console.error(err)
+		  return
+		}
+		console.log(connectString);
+		const db = client.db(dbName);
+		const collection = db.collection('rides');
+		collection.find().toArray((err, items) => {
+			ridesList = items;
+			console.log(items);
+		});
+	  })
+	  console.log(ridesList);		
 	res.send({success: true, result: ridesList});
 
 });
