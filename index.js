5f93f720429df183ed3ee72edbcc6fb88028cd57
jankleinert <jkleiner@redhat.com>
2019-09-05 09:49:14 -0400

    Updated index.js to expect connection string

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 const host = process.env.IP  || '0.0.0.0';
 const port = process.env.PORT || 8080;
-const dbUri = process.env.uri || 'mongodb://localhost:27017';
-const dbName = process.env.database_name || 'sampledb';
-const dbUser = process.env.username;
-const dbPassword = process.env.password;
+const dbConnectionUrl = process.env.MONGODB_URL || 'mongodb://localhost:27017/sampledb';
+const dbName = process.env.MONGODB_DBNAME || 'sampledb';
 const mongo = require('mongodb').MongoClient;
 
 app.get('/ticketNumber', function(req, res, next) {
@@ -14,23 +12,14 @@ app.get('/ticketNumber', function(req, res, next) {
 
 app.get('/initdb', function (req, res, next) {
 	var ridesList;
-	let connectString = dbUri; 
-	console.log(dbName);
-	console.log('connect string: ' + connectString);
-	if (dbUser && dbPassword) {
-		console.log('have username and password ' + dbUser + ' ' + dbPassword);
-		connectString = dbUri.replace('mongodb://', 'mongodb://' + dbUser + ':' + dbPassword + '@');
-		connectString += '/' + dbName;
-		console.log('connect string after replace: ' + connectString);
-		
-	}
-	console.log('connect string after if ' + connectString);
-	mongo.connect(connectString, (err, client) => {
+	console.log(dbConnectionUrl);
+
+	mongo.connect(dbConnectionUrl, (err, client) => {
 		if (err) {
 		  console.error(err)
 		  return
 		}
-		console.log(connectString);
+		console.log(dbConnectionUrl);
 		const db = client.db(dbName);
 		const collection = db.collection('rides');
 		collection.insertMany([{id: '123', name: 'Compile Driver', wait: 30}, {id: '234', name: 'Wild West', wait: 5}], (err, result) => {
@@ -49,19 +38,13 @@ app.get('/initdb', function (req, res, next) {
 
 app.get('/allrides/', function (req, res, next) {
 	var ridesList;
-	let connectString = dbUri;
-	if (dbUser && dbPassword) {
-		console.log('have username and password ' + dbUser + ' ' + dbPassword);
-		connectString = dbUri.replace('mongodb://', 'mongodb://' + dbUser + ':' + dbPassword + '@');
-		connectString += '/' + dbName;
-		console.log(connectString);
-	}
-	mongo.connect(connectString, (err, client) => {
+
+	mongo.connect(dbConnectionUrl, (err, client) => {
 		if (err) {
 		  console.error(err)
 		  return
 		}
-		console.log(connectString);
+		console.log(dbConnectionUrl);
 		const db = client.db(dbName);
 		const collection = db.collection('rides');
 		collection.find().toArray((err, items) => {
@@ -80,4 +63,4 @@ app.use(function(err, req, res, next) {
 });
 
 app.listen(port, host);
-console.log('Wait Tracker Backend started on: ' + host + ':' + port);
+console.log('Concession Kiosk Backend started on: ' + host + ':' + port);
