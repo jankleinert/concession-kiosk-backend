49dfefd3975f1102f2dfd03de8af24249b8ab135
Zhao Li <zhao.l.li.civ@us.navy.mil>
2024-02-26 10:40:28 -1000

    remove reference code that is no longer needed

--- index.js
+++ index.js
@@ -65,31 +65,6 @@ app.get('/ticketNumber', function(req, res, next) {
 				//});
 				//res.send({success: true, result: newTicketNumber, order: req.query});
 			}
-
-			//await collection.find({}).count().then((n) => {
-      //  console.log(".find({}).count()");
-      //  console.log(n);
-			//	if (n > 0) {
-      //    console.log("n > 0");
-			//		//collection.find().sort({ticketNumber:-1}).limit(1).toArray((err, items) => {
-			//		collection.find().toArray((err, items) => {
-      //      console.log(items)
-			//		});
-			//		//collection.find().sort({ticketNumber:-1}).limit(1).toArray((err, items) => {
-			//		//	let highestTicket = items[0].ticketNumber;
-			//		//	newTicketNumber = highestTicket + 1;
-			//		//	collection.insertOne({ticketNumber: newTicketNumber, order: req.query}, (err, result) => {
-			//		//		console.log('err:' + err, ' result: ' + result);
-			//		//	});
-			//		//	res.send({success: true, result: newTicketNumber, order: req.query});
-			//		//});
-			//	} else {
-			//		//collection.insertOne({ticketNumber: newTicketNumber, order: req.query}, (err, result) => {
-			//		//	console.log('err:' + err, ' result: ' + result);
-			//		//});
-			//		//res.send({success: true, result: newTicketNumber, order: req.query});
-			//	}
-      //});
     } finally {
       await client.close()
     }
